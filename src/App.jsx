a6791ce0597ba39596e6cffd5045207c747efc13
yasas_ravishanka <ravishanka.yasas@gmail.com>
2026-08-16 19:46:28 +0530

    base project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      this is the main page
+    </main>
+  )
+}
+
+export default App;
